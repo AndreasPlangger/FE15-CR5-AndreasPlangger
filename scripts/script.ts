b78f72672ddf1d06e98c4f7038620f682ab0b216
AndreasPlangger <andreas_plangger@hotmail.com>
2022-02-11 23:21:33 +0100

    add vax color

--- scripts/script.ts
+++ scripts/script.ts
@@ -18,16 +18,15 @@ class Animal {
     displayTop() {
         return `
         <div class = "col">
-        <div class="card" style="width: 18rem;">
+        <div class="card h-100 shadow mb-5 bg-body rounded" style="width: 18rem;">
         <img src="${this.img}" class="card-img-top" alt="...">
         <div class="card-body">
-        <h5 class = "cart-title"> ${this.name}</h5>
-        <h6 class = "cart-title"> ${this.gender}</h6> 
-        <h6 class = "cart-title"> ${this.age}</h6>
-        <h6 class = "cart-title"> ${this.size}</h6>
-        <h6 class = "cart-title"> ${this.vaccine}</h6>  
-`
-
+        <h5 class = "cart-title text-center text-light bg-dark"> ${this.name}</h5>
+        <h6 class = "cart-title">Gender: ${this.gender}</h6> 
+        <h6 class = "cart-title">Age: ${this.age}</h6>
+        <h6 class = "cart-title">Size: ${this.size}</h6>
+        <h6 class = "cart-title bg-${this.vaccine ? "success" : "danger"} text-center p-2"> ${this.vaccine}</h6>
+        `
     }
     displayBottom() {
         return `        
@@ -57,9 +56,9 @@ class Cat extends Animal {
     displayTop() {
         return `
         ${super.displayTop()}
-        <h6 class = "cart-title"> ${this.breed}</h6>
-        <h6 class = "cart-title"> ${this.furColor}</h6>
-        <h6 class = "cart-title"> ${this.urlBreed}</h6>`;
+        <h6 class = "cart-title">Breed: ${this.breed}</h6>
+        <h6 class = "cart-title">Fur color: ${this.furColor}</h6>
+        <h6 class = "cart-title">Learn more: ${this.urlBreed}</h6>`;
 
     }
 }
@@ -74,21 +73,23 @@ class Dog extends Animal {
     }
     displayTop() {
         return `${super.displayTop()}
-        <h6 class = "cart-title"> ${this.breed}</h6>
-        <h6 class = "cart-title"> ${this.training}</h6>`;
+        <h6 class = "cart-title">Breed: ${this.breed}</h6>
+        <h6 class = "cart-title">Training: ${this.training}</h6>`;
     }
 
 }
 
 let animals: Array<Animal> = [];
 
-new Animal("Rocky", "Male", 4, "medium", true, "img/turtle_640.jpg");
-new Animal("Roger", "Male", 2, "small", true, "img/rabbit_640.jpg");
-new Cat("Fidel Catstro ", "Male", 8, "small", false, "img/cat_640.jpg", "russian-blue", "silver-gray", "https://en.wikipedia.org/wiki/Russian_Blue");
-new Cat("Cat2", "Female", 7, "small", true, "img/cat2_640.jpg", "Birman", "white", "https://en.wikipedia.org/wiki/Birman");
-new Dog("Dog1", "Male", 2, "medium", true, "img/dog_640.jpg", "Bullterrier", true);
-new Dog("Dog2", "Female", 5, "large", true, "img/dog2_640.jpg", "Husky", true);
-
+new Animal("Donnashello", "Male", 28, "medium", true, "img/turtle_640.jpg");
+new Animal("Rabbit DeNiro", "Male", 2, "small", true, "img/rabbit_640.jpg");
+new Animal("Marty McFly", "Male", 2, "small", true, "img/parrott_640.jpg");
+new Cat("Fidel Catstro", "Male", 8, "small", false, "img/cat_russian_blue.jpg", "russian-blue", "silver-gray", "en.wikipedia.org/wiki/Russian_Blue");
+new Cat("Cleocatra", "Female", 7, "small", true, "img/cat_birman.jpg", "Birman", "white", "en.wikipedia.org/wiki/Birman");
+new Cat("Cat Stevens", "Female", 4, "small", true, "img/cat_scotish.jpg", "Scottish Fold", "white", "en.wikipedia.org/wiki/Scottish_Fold");
+new Dog("Dogstoyevsky", "Male", 2, "medium", true, "img/dog_640.jpg", "Bullterrier", true);
+new Dog("Karl Barx", "Male", 5, "large", true, "img/dog_terrier.jpg", "Terrier", true);
+new Dog("Woofi Goldberg", "Female", 6, "large", false, "img/dog2_640.jpg", "Husky", true);
 animals.forEach((val) => {
     (document.getElementById("row") as HTMLElement).innerHTML += val.display();
 })
